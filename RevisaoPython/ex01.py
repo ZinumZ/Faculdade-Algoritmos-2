b56b7ad0fc7297575d39040768d899ac5f7ac31a
Anderson <anderson.muniz8@gmail.com>
2023-06-14 20:34:13 -0400

    Comentarios

---
+++ RevisaoPython/ex01.py
@@ -0,0 +1,6 @@
+print("Hello World!", "Teste", sep="+") #No lugar do espaço entre as strings ele coloca "+"
+print("Teste", end="\n\n") #Substitue a implementação automática do \n
+print("Teste", end="") 
+'''
+3 aspas simples configura um comentário também
+''' 
